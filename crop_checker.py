3d00feced020f2e72a6279c1a613fe5751b571c9
Manas Bhardwaj <mb.manasbhardwaj@gmail.com>
2020-03-23 18:09:55 +0530

    data loading work initiate

---
+++ crop_checker.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 20:48:57 2020
+
+@author: manas
+"""
+
---
+++ data_loader_in_numpy_array.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 22:45:06 2020
+
+@author: manas
+"""
+
