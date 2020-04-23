5c1555ca2f103bafc442626077e3f1c8d53f9012
Manas Bhardwaj <mb.manasbhardwaj@gmail.com>
2020-04-23 22:42:22 +0530

    Server code updated

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ import requests
 import base64
 import json
 #import sys
-from flask import Flask, render_template, request, jsonify, request, send_from_directory
+from flask import Flask, render_template, jsonify, request, send_from_directory
 from flask_restful import Resource, Api, reqparse
 import os
 import random
@@ -19,22 +19,22 @@ import cv2
 import datetime
 from keras.preprocessing import image
 import numpy as np
+from keras.models import load_model
+
 
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = 'QuarterNibble'
-#api = Api(app)
-
-# Loading model and predict.
-from keras.models import load_model
-model=load_model('Ignore/crop.h5')
-
-Classes = ["Potato___Early_blight","Potato___Late_blight","Potato___healthy","Tomato___Bacterial_spot","Tomato___Early_blight","Tomato___Late_blight","Tomato___Leaf_Mold","Tomato___Septoria_leaf_spot","Tomato___Spider_mites Two-spotted_spider_mite","Tomato___Target_Spot","Tomato___Tomato_mosaic_virus","Tomato___healthy"]
-
+app.config['SECRET_KEY'] = 'QuarterNibble'
+api = Api(app)
 
 # Folder where the image received from android will be saved.
 UPLOAD_FOLDER = os.path.basename('Uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Loading model and predict.
+model=load_model('Ignore/crop.h5')
+
+Classes = ["Potato___Early_blight","Potato___Late_blight","Potato___healthy","Tomato___Bacterial_spot","Tomato___Early_blight","Tomato___Late_blight","Tomato___Leaf_Mold","Tomato___Septoria_leaf_spot","Tomato___Spider_mites Two-spotted_spider_mite","Tomato___Target_Spot","Tomato___Tomato_mosaic_virus","Tomato___healthy"]
 
 
 ######################### APP ROUTES ##############################
@@ -48,11 +48,13 @@ def home():
 
 # Function to convert image from base64 format (from android POST request) to bytes object and then saving it in jpg format to the disk.  
 def convertImage(imgData1, upload_loc):
-	with open( upload_loc, "wb") as output:
-		output.write(base64.b64decode(imgData1))
+    print(1)
+    with open(upload_loc, "wb") as output:
+        output.write(base64.b64decode(imgData1))
 
 def prepare(img_path):
     img = image.load_img(img_path, target_size=(256, 256))
+#    img = image.res
     x = image.img_to_array(img)
     x = x/255
     return np.expand_dims(x, axis=0)
@@ -64,18 +66,53 @@ def predict():
     '''
     For rendering results on HTML
     '''
-    imgData = request.get_json()
-    print(1)
-    currentDT = datetime.datetime.now()    
-    random_string = currentDT.strftime('%m/%d/%Y')
-    upload_loc = os.path.join(UPLOAD_FOLDER, random_string + ".jpg")
-    convertImage(imgData, upload_loc)
-#    result = model.predict_classes([prepare('/content/drive/My Drive/Test_D/Tomato_BacterialSpot/Tomato___Bacterial_spot(901).JPG')])
-    result = model.predict_classes([prepare(imgData)])
-    pred_disease = Classes[int(result)]
+
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            print(file.filename)
+            print(file)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            upload_loc = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            print(upload_loc)
+            img = cv2.imread(upload_loc, 0)
+            cv2.imshow('image', img)
+#            result = model.predict_classes([prepare(upload_loc)])
+#            pred_disease = Classes[int(result)]
+#            return render_template('index.html', prediction='Predicted disease is ${}'.format(pred_disease))
+    return "No file entered"
+            
+
+#    value = request.files['file']
+#    for key, value in request.files('file'):
+#        print(key)
+#        print(type(value))
+#    data = request.form.get()
+#    imgData = request.get_json()
+#    print(imgData)
+#    print(type)
+#    print(1)
+#    currentDT = datetime.datetime.now()    
+#    random_string = currentDT.strftime('%m/%d/%Y')
+#    random_string = "mb";
+#    upload_loc = os.path.join(UPLOAD_FOLDER, random_string + ".jpg")
+#    imgData = data['file']
+#    print(key)
+#    print(value)
+#    convertImage(imgData, upload_loc)
+#    result = model.predict_classes([prepare(imgData)])
+#    r = request
+#    print(r.get_json())
+#    # convert string of image data to uint8
+#    nparr = np.frombuffer(r.data, np.uint8)
+#    # decode image
+#    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+#    img = cv2.resize(img, (256,256))
+#    result = model.predict_classes([prepare(img)])
+#    pred_disease = Classes[int(result)]
     
     return render_template('index.html', prediction='Predicted disease is ${}'.format(pred_disease))
-
+#    return "MB"
 
 ## POST
 #@app.route('/uploadform', methods=['POST'])
